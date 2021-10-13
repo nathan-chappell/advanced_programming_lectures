a88ee605f75c68a612c869c25d7487a80c864205
Nathan Chappell <nchappell@mono-software.com>
2021-10-13 08:09:49 +0100

    solution 1

--- dependency_breaking_techniques/examples/test_override/test_report_generator.py
+++ dependency_breaking_techniques/examples/test_override/test_report_generator.py
@@ -1,7 +1,22 @@
 from report_generator import ReportGenerator
 
+class TestReportGenerator(ReportGenerator):
+    def __init__(self):
+        ...
+
+    def report_data(self, report):
+        self.report = report
+
 def test_analyze_data():
     data = [1,2,3,4]
     # verify:
     #  mean = 2.5, var = 1.25
-
+    report_generator = TestReportGenerator()
+    report_generator.analyze_data('test-data', data)
+    mean = report_generator.report['mean']
+    var = report_generator.report['var']
+    assert mean == 2.5
+    assert var == 1.25
+    
+if __name__ == '__main__':
+    test_analyze_data()
