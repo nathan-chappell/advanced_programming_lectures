e2ecc00d7060315a680196482ef199db7f33eff3
Nathan Chappell <nchappell@mono-software.com>
2021-10-13 08:05:18 +0100

    python report_generator and test case

---
+++ dependency_breaking_techniques/examples/test_override/report_generator.py
@@ -0,0 +1,24 @@
+class ReportSaver:
+    def __init__(self):
+        raise NotImplementedError()
+    
+    def save_report(self, report):
+        raise NotImplementedError()
+    
+class ReportGenerator:
+    def __init__(self):
+        self.report_saver = ReportSaver()
+
+    def make_report(self, name, mean, var):
+        report = {'name': name, 'mean': mean, 'var': var}
+        return report
+
+    def analyze_data(self, name, data):
+        mean = sum(data) / len(data)
+        var = sum([x**2 for x in data]) - mean**2
+        report = self.make_report(name, mean, var)
+        self.report_data(report)
+
+    def report_data(self, report):
+        self.report_saver.save_report(report)
+
---
+++ dependency_breaking_techniques/examples/test_override/test_report_generator.py
@@ -0,0 +1,7 @@
+from report_generator import ReportGenerator
+
+def test_analyze_data():
+    data = [1,2,3,4]
+    # verify:
+    #  mean = 2.5, var = 1.25
+
